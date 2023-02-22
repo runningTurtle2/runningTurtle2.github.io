de4b6f33902ee4858ba7427e347da677cec54d62
runningTurtle2 <118147333+runningTurtle2@users.noreply.github.com>
2023-02-21 16:12:25 -0800

    Updated

--- Artifact03/script.js
+++ Artifact03/script.js
@@ -8,25 +8,30 @@ function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
 
     function upLeft(pHeight, pColorEven, pColorOdd, pSymbol){
 
-    var lLine ="";
-    for (i = 0; i < pHeight; i++){
-    lLine +="<p>";
-    //Create each line on the Rhombus
-    for(j = 0; j <= i; j++){
-    //Is the position even or odd so we change the color
-    if (j%2)
-    //even
-    lLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
-    else
-    //odd
-    lLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
+    var rLine ="";
+    for (i = 1; i <= pHeight; i++){
+      rLine +="<p>";
+      
+      for(x=1; x<=pHeight-i; x++){
+        rLine +="-";
+      }
+      
+      //Create each line on the Rhombus
+      for(j = x; j <= pHeight; j++){
+        //Is the position even or odd so we change the color
+        if (j%2)
+        //even
+        rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
+        else
+        //odd
+        rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
     
-    }
-    lLine +="</p>";
-    // console.log(lLine);
+        }
+      rLine +="</p>";
+    // console.log(rLine);
     }
     
-    document.getElementById("upLeft").innerHTML = lLine;
+    document.getElementById("upLeft").innerHTML = rLine;
     }
 
     function downLeft(pHeight, pColorEven, pColorOdd, pSymbol){
