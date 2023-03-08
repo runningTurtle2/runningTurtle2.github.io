db40c8a0eddf0e18003ddb161bca6fb6359589d8
runningTurtle2 <oscarmoren2010@gmail.com>
2023-03-07 21:40:07 -0800

    Fixed issue validation

--- Final/assets/reaction.js
+++ Final/assets/reaction.js
@@ -2,30 +2,33 @@ function validForm(){
 // Create variable
 var validName = false;
 var validEmail = false;
-var errorMessages = "";
+var errorName = "";
+var errorEmail = "";
+var letters =/^[A-Za-z]+$/;
 
 // Read value from html
 var userEmail = document.getElementById("email").value;
 var atpos = userEmail.indexOf("@");
 var dotpos = userEmail.lastIndexOf(".");
-var name = document.getElementById("name");
+var name = document.getElementById("name").value;
 
 
-if (firstname==="null" || firstname==="" || firstname.length > 20 || !firstname.match(letters)){
-    errorMessages += "<p>The first name is required and cannot be greater than 20 characters. Only letters are accepted.</p>";
+if (name==="null" || name==="" || name.length > 20 || !name.match(letters)){
+    errorName = "<p>The first name is required and cannot be greater than 20 characters. Only letters are accepted.</p>";
 }
 else{
     validName = true;
 }
 
 if (atpos< 1 || dotpos < atpos+2 || dotpos+2 >= userEmail.length) {
-    errorMessages += "<p>Invalid email</p>";
+    errorEmail = "<p>Invalid email</p>";
 }
 else{
     validEmail = true; 
 }
 
-document.getElementById("errorMessage").innerHTML = errorMessages;
+document.getElementById("errorName").innerHTML = errorName;
+document.getElementById("errorEmail").innerHTML = errorEmail;
 // testing
 console.log(validEmail)
 console.log(validName)
